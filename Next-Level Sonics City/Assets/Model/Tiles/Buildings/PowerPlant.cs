79d867b5f049065ebf143aa374cd133488a01ee0
VGBence24 <55708130+VGBence24@users.noreply.github.com>
2023-03-28 17:48:21 +0200

    IWorkplace development

--- Next-Level Sonics City/Assets/Model/Tiles/Buildings/PowerPlant.cs
+++ Next-Level Sonics City/Assets/Model/Tiles/Buildings/PowerPlant.cs
@@ -1,3 +1,5 @@
+using Model;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Tiles;
@@ -5,8 +7,44 @@ using UnityEngine;
 
 namespace Buildings
 {
-    public class PowerPlant : Building
+    public class PowerPlant : Building, IWorkplace
     {
+        private List<Person> _workers;
+        private int _workersLimit;
 
+        public PowerPlant()
+        {
+
+        }
+
+        public bool Employ(Person person)
+        {
+            if (_workers.Count < _workersLimit)
+            {
+                _workers.Add(person);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Fire(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetWorkers()
+        {
+            return _workers;
+        }
+
+        public int GetWorkersCount()
+        {
+            return _workers.Count;
+        }
+        public int GetWorkersLimit()
+        {
+            return _workersLimit;
+        }
     }
 }
--- Next-Level Sonics City/Assets/Model/Tiles/Buildings/Stadion.cs
+++ Next-Level Sonics City/Assets/Model/Tiles/Buildings/Stadion.cs
@@ -2,11 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Tiles;
+using Model;
+using System;
 
 namespace Buildings
 {
-    public class Stadion : Building
+    public class Stadion : Building, IWorkplace
     {
+        private List<Person> _workers;
+        private int _workersLimit;
 
+        public Stadion()
+        {
+
+        }
+
+        public bool Employ(Person person)
+        {
+            if (_workers.Count < _workersLimit)
+            {
+                _workers.Add(person);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Fire(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetWorkers()
+        {
+            return _workers;
+        }
+
+        public int GetWorkersCount()
+        {
+            return _workers.Count;
+        }
+        public int GetWorkersLimit()
+        {
+            return _workersLimit;
+        }
     }
 }
