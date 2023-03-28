ff36e10d9c09a7986323ca32e052370f82645859
VGBence24 <55708130+VGBence24@users.noreply.github.com>
2023-03-28 17:54:20 +0200

    IZoneBuilding development

--- Next-Level Sonics City/Assets/Model/IWorkplace.cs
+++ Next-Level Sonics City/Assets/Model/IWorkplace.cs
@@ -6,14 +6,10 @@ namespace Model
 {
     public interface IWorkplace
     {
-<<<<<<< Updated upstream
-
-=======
         public bool Employ(Person person);
         public void Fire(Person person);
         public List<Person> GetWorkers();
         public int GetWorkersCount();
         public int GetWorkersLimit();
->>>>>>> Stashed changes
     }
 }
