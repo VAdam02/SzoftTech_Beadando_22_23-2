594d3c826690615800ea93d0c53a7d3fa4a767f8
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-04-02 01:09:23 +0200

    Updating spaces to tabs

--- Next-Level Sonics City/Assets/Model/Tiles/Building.cs
+++ Next-Level Sonics City/Assets/Model/Tiles/Building.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 namespace Model.Tiles
 {
-    public abstract class Building : Tile
-    {
-        public int ID { get; private set; }
-        public int Helath { get; private set; }
-        public bool IsOnFire { get; private set; }
-        public List<Road> ConnectedTo { get; private set; }
+	public abstract class Building : Tile
+	{
+		public int ID { get; private set; }
+		public int Helath { get; private set; }
+		public bool IsOnFire { get; private set; }
+		public List<Road> ConnectedTo { get; private set; }
 
-        public bool StartFire()
-        {
-            throw new NotImplementedException();
-        }
+		public bool StartFire()
+		{
+			throw new NotImplementedException();
+		}
 
-        public void GetFirePercentage()
-        {
-            throw new NotImplementedException();
-        }
+		public void GetFirePercentage()
+		{
+			throw new NotImplementedException();
+		}
 
 	}
 }
