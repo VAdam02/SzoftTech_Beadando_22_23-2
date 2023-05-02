f26a497f49e47779af3f12d8fc5a3da0c56c4995
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-05-02 16:49:50 +0200

    merge conflict resolve

--- Next-Level Sonics City/Assets/Model/Tiles/Buildings/PowerPlant.cs
+++ Next-Level Sonics City/Assets/Model/Tiles/Buildings/PowerPlant.cs
@@ -117,6 +117,54 @@ namespace Model.Tiles.Buildings
 			return true;
 		}
 
+		internal override bool IsExpandable()
+		{
+			return true;
+		}
+
+		internal override bool CanExpand()
+		{
+			int x1 = (int)Coordinates.x;
+			int y1 = (int)Coordinates.y;
+			int x2 = (int)Coordinates.x;
+			int y2 = (int)Coordinates.y;
+
+
+			switch (Rotation)
+			{
+				case Rotation.Zero:
+					x1 += 1; y1 += 1;
+					break;
+				case Rotation.Ninety:
+					x1 += -1; y1 += 1;
+					break;
+				case Rotation.OneEighty:
+					x1 += -1; y1 += 1;
+					break;
+				case Rotation.TwoSeventy:
+					x1 += 1; y1 += -1;
+					break;
+			}
+
+			int minX = Math.Min(x1, x2);
+			int maxX = Math.Max(x1, x2);
+			int minY = Math.Min(y1, y2);
+			int maxY = Math.Max(y1, y2);
+
+			for (int i = minX; i < maxX; ++i)
+			{
+				for (int j = minY; j < maxY; ++j)
+				{
+					if (SimEngine.Instance.GetTile(i, j) is not EmptyTile)
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
 		internal override void Expand()
 		{
 			int x1 = (int)Coordinates.x;
