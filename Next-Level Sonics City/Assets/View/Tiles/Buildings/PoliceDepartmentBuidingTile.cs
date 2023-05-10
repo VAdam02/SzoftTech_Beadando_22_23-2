a829840fc2783ebc38b396e6878ac9dcd5ce764f
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-05-10 18:02:09 +0200

    Police department display

---
+++ Next-Level Sonics City/Assets/View/Tiles/Buildings/PoliceDepartmentBuidingTile.cs
@@ -0,0 +1,153 @@
+using Model.Tiles;
+using UnityEngine;
+
+namespace View.Tiles.Buildings
+{
+	public class PoliceDepartmentBuidingTile : Tile
+    {
+		private static Material _grassMaterial;
+		public static Material GrassMaterial { get { if (_grassMaterial == null) _grassMaterial = LoadMaterialByName("AsphaltMaterial"); return _grassMaterial; } }
+		private Material _sharedGrassMaterial;
+		private Material SharedGrassMaterial
+		{
+			get
+			{
+				if (_sharedGrassMaterial == null)
+				{
+					_sharedGrassMaterial = new Material(GrassMaterial);
+					_materials.Add(_sharedGrassMaterial);
+				}
+				return _sharedGrassMaterial;
+			}
+		}
+
+		private static Material _windowMaterial;
+		public static Material WindowMaterial { get { if (_windowMaterial == null) _windowMaterial = LoadMaterialByName("SidewalkMaterial"); return _windowMaterial; } }
+		private Material _sharedWindowMaterial;
+		private Material SharedWindowMaterial
+		{
+			get
+			{
+				if (_sharedWindowMaterial == null)
+				{
+					_sharedWindowMaterial = new Material(WindowMaterial);
+					_materials.Add(_sharedWindowMaterial);
+				}
+				return _sharedWindowMaterial;
+			}
+		}
+
+		private static Material _baseMaterial;
+		public static Material BaseMaterial { get { if (_baseMaterial == null) _baseMaterial = LoadMaterialByName("SidewalkMaterial"); return _baseMaterial; } }
+		private Material _sharedBaseMaterial;
+		private Material SharedBaseMaterial
+		{
+			get
+			{
+				if (_sharedBaseMaterial == null)
+				{
+					_sharedBaseMaterial = new Material(BaseMaterial);
+					_materials.Add(_sharedBaseMaterial);
+				}
+				return _sharedBaseMaterial;
+			}
+		}
+
+		private static Material _towerBlueMaterial;
+		public static Material TowerBlueMaterial { get { if (_towerBlueMaterial == null) _towerBlueMaterial = LoadMaterialByName("SidewalkMaterial"); return _towerBlueMaterial; } }
+		private Material _sharedTowerBlueMaterial;
+		private Material SharedTowerBlueMaterial
+		{
+			get
+			{
+				if (_sharedTowerBlueMaterial == null)
+				{
+					_sharedTowerBlueMaterial = new Material(TowerBlueMaterial);
+					_materials.Add(_sharedTowerBlueMaterial);
+				}
+				return _sharedTowerBlueMaterial;
+			}
+		}
+
+		private static Material _helipadMaterial;
+		public static Material HelipadMaterial { get { if (_helipadMaterial == null) _helipadMaterial = LoadMaterialByName("SidewalkMaterial"); return _helipadMaterial; } }
+		private Material _sharedHelipadMaterial;
+		private Material SharedHelipadMaterial
+		{
+			get
+			{
+				if (_sharedHelipadMaterial == null)
+				{
+					_sharedHelipadMaterial = new Material(HelipadMaterial);
+					_materials.Add(_sharedHelipadMaterial);
+				}
+				return _sharedHelipadMaterial;
+			}
+		}
+
+		private static Material _helipadHMaterial;
+		public static Material HelipadHMaterial { get { if (_helipadHMaterial == null) _helipadHMaterial = LoadMaterialByName("SidewalkMaterial"); return _helipadHMaterial; } }
+		private Material _sharedHelipadHMaterial;
+		private Material SharedHelipadHMaterial
+		{
+			get
+			{
+				if (_sharedHelipadHMaterial == null)
+				{
+					_sharedHelipadHMaterial = new Material(HelipadHMaterial);
+					_materials.Add(_sharedHelipadHMaterial);
+				}
+				return _sharedHelipadHMaterial;
+			}
+		}
+
+		private static Material LoadMaterialByName(string name)
+		{
+			return Resources.Load<Material>("Tiles/RoadTile/Material/" + name);
+		}
+
+		private void SetSharedMaterials(Renderer renderer)
+		{
+			if (renderer == null) return;
+
+			Material[] materials = renderer.sharedMaterials;
+
+			for (int i = 0; i < materials.Length; i++)
+			{
+				if		(materials[i].name.Split(' ')[0] == "WindowMaterial") { materials[i] = SharedGrassMaterial; }
+				else if (materials[i].name.Split(' ')[0] == "BaseMaterial") { materials[i] = SharedWindowMaterial; }
+				else if (materials[i].name.Split(' ')[0] == "TowerBlueMaterial") { materials[i] = SharedTowerBlueMaterial; }
+				else if (materials[i].name.Split(' ')[0] == "HelipadMaterial") { materials[i] = SharedHelipadMaterial; }
+				else if (materials[i].name.Split(' ')[0] == "HelipadHMaterial") { materials[i] = SharedHelipadHMaterial; }
+				else
+				{
+					Debug.LogWarning(renderer);
+					Debug.LogError("Unknown material found: " + renderer.materials[i].name);
+				}
+			}
+
+			renderer.sharedMaterials = materials;
+		}
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			transform.localPosition = new Vector3(TileModel.Coordinates.x, 0, -TileModel.Coordinates.y) * 10;
+			transform.localScale = Vector3.one;
+			transform.localRotation = Quaternion.Euler(0, ((int)((Building)TileModel).Rotation) * 90, 0);
+
+			SetSharedMaterials(gameObject.GetComponent<Renderer>());
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		private static GameObject LoadModelByName(string name)
+		{
+			return Resources.Load<GameObject>("Tiles/PoliceDepartmentBuildingTile/Model/" + name);
+		}
+	}
+}
