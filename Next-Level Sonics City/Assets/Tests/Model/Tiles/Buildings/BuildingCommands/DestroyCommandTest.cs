52cc5cfa028e2ebd2d73af626a384b93f982cbd5
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-05-17 01:04:49 +0200

    Model.Tile.Buildings.BuildingCommands finished

---
+++ Next-Level Sonics City/Assets/Tests/Model/Tiles/Buildings/BuildingCommands/DestroyCommandTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace Model.Tiles.Buildings.BuildingCommands
+{
+	public class DestroyCommandTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			City.Reset();
+		}
+
+		[Test]
+		public void Execute_DestroyTile_RemovesTileFromCity()
+		{
+			int x = 3;
+			int y = 4;
+			City.Instance.SetTile(new MockRoadGridElement(x, y));
+
+			Assert.IsNotInstanceOf<EmptyTile>(City.Instance.GetTile(x, y));
+
+			DestroyCommand command = new(x, y);
+			command.Execute();
+
+			Assert.IsInstanceOf<EmptyTile>(City.Instance.GetTile(x, y));
+		}
+	}
+}
