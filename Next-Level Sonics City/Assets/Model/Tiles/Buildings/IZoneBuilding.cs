f4270d0e876c60e840300f9fd19053da2234b2f4
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-05-17 10:12:43 +0200

    MModel.Tiles.BuildingManager finished

--- Next-Level Sonics City/Assets/Model/Tiles/Buildings/IZoneBuilding.cs
+++ Next-Level Sonics City/Assets/Model/Tiles/Buildings/IZoneBuilding.cs
@@ -2,6 +2,27 @@ namespace Model.Tiles.Buildings
 {
 	public interface IZoneBuilding
 	{
+		/// <summary>
+		/// Returns the type of zone this ZoneBuilding
+		/// </summary>
+		/// <returns></returns>
+		public abstract ZoneType GetZoneType();
+
+		/// <summary>
+		/// Level up the building
+		/// </summary>
 		public abstract void LevelUp();
+
+		/// <summary>
+		/// Returns the cost of leveling up the building
+		/// </summary>
+		public abstract int GetLevelUpCost();
+
+		/// <summary>
+		/// Returns the current level of the building
+		/// </summary>
+		public abstract ZoneBuildingLevel Level { get; }
+
+		public abstract Tile GetTile();
 	}
 }
---
+++ Next-Level Sonics City/Assets/Tests/Model/Tiles/Buildings/BuildingManagerTest.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+namespace Model.Tiles.Buildings
+{
+	public class BuildingManagerTests
+	{
+		[SetUp]
+		public void Setup()
+		{
+			City.Reset();
+		}
+
+		[Test]
+		public void Build_ValidEmptyTile_BuildsBuildingAndRaisesBuildingBuiltEvent()
+		{
+			Tile tile = new EmptyTile(3, 4, 123);
+			TileType tileType = TileType.PoliceDepartment;
+			Rotation rotation = Rotation.Zero;
+
+			BuildingManager manager = BuildingManager.Instance;
+			bool eventRaised = false;
+			manager.BuildingBuilt += (sender, e) => eventRaised = true;
+
+			manager.Build(tile, tileType, rotation);
+
+			Tile createdTile = City.Instance.GetTile(3, 4);
+			Assert.NotNull(createdTile);
+			Assert.AreEqual(tileType, createdTile.GetTileType());
+			Assert.True(eventRaised);
+		}
+
+		[Test]
+		public void Build_InvalidNonEmptyTile_DoesNotBuildBuildingAndDoesNotRaiseBuildingBuiltEvent()
+		{
+			int x = 3;
+			int y = 4;
+			City.Instance.SetTile(new PoliceDepartmentBuildingTile(x, y, 123, Rotation.Zero));
+			TileType tileType = TileType.PoliceDepartment;
+			Rotation rotation = Rotation.Zero;
+
+			BuildingManager manager = BuildingManager.Instance;
+			bool eventRaised = false;
+			manager.BuildingBuilt += (sender, e) => eventRaised = true;
+
+			manager.Build(City.Instance.GetTile(x, y), tileType, rotation);
+
+			Tile createdTile = City.Instance.GetTile(3, 4);
+			Assert.IsInstanceOf<PoliceDepartmentBuildingTile>(createdTile);
+			Assert.False(eventRaised);
+		}
+
+		[Test]
+		public void Destroy_ValidNonEmptyTile_DestroysBuildingAndDoesNotRaiseBuildingBuiltEvent()
+		{
+			Tile tile = new PoliceDepartmentBuildingTile(3, 4, 123, Rotation.Zero);
+
+			BuildingManager manager = BuildingManager.Instance;
+			bool eventRaised = false;
+			manager.BuildingBuilt += (sender, e) => eventRaised = true;
+
+			manager.Destroy(tile);
+
+			Tile destroyedTile = City.Instance.GetTile(3, 4);
+			Assert.NotNull(destroyedTile);
+			Assert.IsTrue(destroyedTile is EmptyTile);
+			Assert.False(eventRaised);
+		}
+	}
+}
