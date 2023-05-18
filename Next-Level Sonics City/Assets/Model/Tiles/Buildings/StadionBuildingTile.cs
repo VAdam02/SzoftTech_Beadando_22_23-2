68990ba653ab81eb4a464bebcd71d612c7966d24
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-05-18 10:01:57 +0200

    All workplace finished

--- Next-Level Sonics City/Assets/Model/Tiles/Buildings/StadionBuildingTile.cs
+++ Next-Level Sonics City/Assets/Model/Tiles/Buildings/StadionBuildingTile.cs
@@ -1,88 +1,103 @@
 using Model.Persons;
-using Model.Simulation;
+using Model.RoadGrids;
 using Model.Tiles.Buildings.BuildingCommands;
 using System;
 using System.Collections.Generic;
-using Model.RoadGrids;
 
 namespace Model.Tiles.Buildings
 {
 	public class StadionBuildingTile : Building, IWorkplace
 	{
 		private readonly List<Worker> _workers = new();
-		private int _workersLimit = 10;
+		public int WorkplaceLimit { get; private set; }
 
 		public StadionBuildingTile(int x, int y, uint designID, Rotation rotation) : base(x, y, designID, rotation)
 		{
 
 		}
 
+		public override void FinalizeTile()
+		{
+			Finalizing();
+		}
+
+		/// <summary>
+		/// <para>MUST BE STARTED WITH <code>base.Finalizing()</code></para>
+		/// <para>Do the actual finalization</para>
+		/// </summary>
+		protected new void Finalizing()
+		{
+			base.Finalizing();
+			//TODO implement stadion workplace limit
+			WorkplaceLimit = 10;
+		}
+
 		public override TileType GetTileType() { return TileType.Stadion; }
 
 		public void RegisterWorkplace(RoadGrid roadGrid)
 		{
+			if (!_isFinalized) { throw new InvalidOperationException(); }
+
 			roadGrid?.AddWorkplace(this);
 		}
 
 		public void UnregisterWorkplace(RoadGrid roadGrid)
 		{
+			if (!_isFinalized) { throw new InvalidOperationException(); }
+
 			roadGrid?.RemoveWorkplace(this);
 		}
 
-		public bool Employ(Worker worker)
+		public void Employ(Worker worker)
 		{
-			if (_workers.Count < _workersLimit)
-			{
-				_workers.Add(worker);
-				return true;
-			}
+			if (!_isFinalized) { throw new InvalidOperationException(); }
 
-			return false;
+			if (_workers.Count >= WorkplaceLimit) { throw new InvalidOperationException("The workplace is full"); }
+			_workers.Add(worker);
 		}
 
-		public bool Unemploy(Worker worker)
+		public void Unemploy(Worker worker)
 		{
-			if (_workers.Count > 0)
-			{
-				_workers.Remove(worker);
-				return true;
-			}
+			if (!_isFinalized) { throw new InvalidOperationException(); }
 
-			return false;
+			_workers.Remove(worker);
 		}
 
 		public List<Worker> GetWorkers()
 		{
+			if (!_isFinalized) { throw new InvalidOperationException(); }
+
 			return _workers;
 		}
 
 		public int GetWorkersCount()
 		{
+			if (!_isFinalized) { throw new InvalidOperationException(); }
+
 			return _workers.Count;
 		}
 
-		public int GetWorkersLimit()
-		{
-			return _workersLimit;
-		}
 		public Tile GetTile() { return this; }
 
-		public override int GetBuildPrice() //TODO implementik logic
+		public override int GetBuildPrice()
 		{
-			return BUILD_PRICE;
+			//TODO implement stadion build price
+			return 100000;
 		}
 
-		public override int GetDestroyPrice()
+		public override int GetDestroyIncome()
 		{
-			return DESTROY_PRICE;
+			//TODO implement stadion destroy income
+			return 100000;
 		}
 
 		public override int GetMaintainanceCost()
 		{
-			return GetBuildPrice() / 10;
+			//TODO implement stadion maintainance cost
+			return 100000;
 		}
 
-		internal override bool CanBuild()
+		public override bool CanBuild()
 		{
 			int x1 = (int)Coordinates.x;
 			int y1 = (int)Coordinates.y;
@@ -115,7 +130,7 @@ namespace Model.Tiles.Buildings
 			{
 				for (int j = minY; j <= maxY; ++j)
 				{
-					if (SimEngine.Instance.GetTile(i, j) is not EmptyTile)
+					if (City.Instance.GetTile(i, j) is not EmptyTile)
 					{
 						return false;
 					}
@@ -158,7 +173,6 @@ namespace Model.Tiles.Buildings
 				for (int j = minY; j <= maxY; ++j)
 				{
 					if (i == (int)Coordinates.x && j == (int)Coordinates.y) { continue; }
-					Tile oldTile = SimEngine.Instance.GetTile(i, j);
 					ExpandCommand ec = new(i, j, this);
 					ec.Execute();
 				}
---
+++ Next-Level Sonics City/Assets/Tests/Model/Tiles/Buildings/StadionBuildingTileTest.cs
@@ -0,0 +1,99 @@
+﻿using Model.Persons;
+using Model.RoadGrids;
+using Model.Tiles.Buildings.BuildingCommands;
+using NUnit.Framework;
+using System;
+
+namespace Model.Tiles.Buildings
+{
+	public class StadionBuildingTilTest
+	{
+		private StadionBuildingTile stadion;
+		private ResidentialBuildingTile residential;
+		private IRoadGridElement roadGridElement;
+
+		[SetUp]
+		public void SetUp()
+		{
+			City.Reset();
+
+			roadGridElement = new MockRoadGridElement(0, 0);
+			City.Instance.SetTile(roadGridElement.GetTile());
+			stadion = new StadionBuildingTile(0, 1, 123, Rotation.Zero);
+			City.Instance.SetTile(stadion.GetTile());
+			residential = new ResidentialBuildingTile(1, 0, 456);
+			City.Instance.SetTile(residential.GetTile());
+		}
+
+		[Test]
+		public void GetTileType_ReturnsStadionBuildingTile()
+		{
+			var tileType = stadion.GetTileType();
+
+			Assert.AreEqual(TileType.Stadion, tileType);
+		}
+
+		[Test]
+		public void RegisterWorkplace_AddsStadionBuildingTileToRoadGrid()
+		{
+			Assert.Contains(stadion, roadGridElement.GetRoadGrid().Workplaces);
+		}
+
+		[Test]
+		public void UnregisterWorkplace_RemovesStadionBuildingTileFromRoadGrid()
+		{
+			DestroyCommand destroy = new((int)stadion.Coordinates.x, (int)stadion.Coordinates.y);
+			destroy.Execute();
+
+			CollectionAssert.DoesNotContain(roadGridElement.GetRoadGrid().Workplaces, stadion);
+		}
+
+		[Test]
+		public void Employ_AddsWorkerToStadionBuildingTile()
+		{
+			var worker = new Worker(residential, stadion, 25, Qualification.HIGH);
+
+			Assert.Contains(worker, stadion.GetWorkers());
+		}
+
+		[Test]
+		public void Unemploy_RemovesWorkerFromStadionBuildingTile()
+		{
+			var worker = new Worker(residential, stadion, 25, Qualification.HIGH);
+			stadion.Unemploy(worker); //TODO illegal way to move out
+
+			CollectionAssert.DoesNotContain(stadion.GetWorkers(), worker);
+		}
+
+		[Test]
+		public void GetWorkers_ReturnsListOfWorkersInStadionBuildingTile()
+		{
+			var worker1 = new Worker(residential, stadion, 25, Qualification.HIGH);
+			var worker2 = new Worker(residential, stadion, 25, Qualification.HIGH);
+
+			var workers = stadion.GetWorkers();
+
+			Assert.Contains(worker1, workers);
+			Assert.Contains(worker2, workers);
+		}
+
+		[Test]
+		public void GetWorkersCount_ReturnsNumberOfWorkersInStadionBuildingTile()
+		{
+			_ = new Worker(residential, stadion, 25, Qualification.HIGH);
+			_ = new Worker(residential, stadion, 25, Qualification.HIGH);
+
+			var count = stadion.GetWorkersCount();
+
+			Assert.AreEqual(2, count);
+		}
+
+		[Test]
+		public void GetTile_ReturnsSelf()
+		{
+			var tile = stadion.GetTile();
+
+			Assert.AreEqual(stadion, tile);
+		}
+	}
+}
