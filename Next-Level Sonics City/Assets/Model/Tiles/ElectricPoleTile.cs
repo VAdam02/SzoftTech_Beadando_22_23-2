2aa46b4efe4f4dcb6c7913f2623a81fd4b9234e9
VGBence24 <55708130+VGBence24@users.noreply.github.com>
2023-06-01 13:49:21 +0200

    ElectricPole Orientation

--- Next-Level Sonics City/Assets/Model/Tiles/ElectricPoleTile.cs
+++ Next-Level Sonics City/Assets/Model/Tiles/ElectricPoleTile.cs
@@ -1,6 +1,11 @@
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using Model.ElectricGrids;
+
 namespace Model.Tiles
 {
-	public class ElectricPoleTile : Tile
+	public class ElectricPoleTile : Tile, IElectricGridElement
 	{
 		#region Tile implementation
 		public override TileType GetTileType() { return TileType.ElectricPole; }
@@ -29,6 +34,172 @@ namespace Model.Tiles
 		#region Common implementation
 		public Tile GetTile() => this;
 		#endregion
+
+		#region IPoleGridElement implementation
+		void IElectricGridElement.RegisterElectricGridElement()
+		{
+			if (!_isFinalized) { throw new InvalidOperationException("Tile is not set in the city"); }
+
+			ElectricGridManager.Instance.AddElectricGridElement(this);
+		}
+
+		void IElectricGridElement.UnregisterElectricGridElement()
+		{
+			if (!_isFinalized) { throw new InvalidOperationException("Tile is not set in the city"); }
+
+			SetElectricGrid(null);
+		}
+
+		private readonly IElectricGridElement[] _poles = new IElectricGridElement[4];
+		List<IElectricGridElement> IElectricGridElement.ConnectsTo { get => _poles.Where(x => x != null).ToList(); }
+
+		//private void RegisterNeighbourTileDeleteListeners() TODO
+		//{
+		//	if (City.Instance.GetTile(Coordinates.x, Coordinates.y - 1) is IElectricGridElement aboveGrid) { ConnectsFromAbove = aboveGrid; }
+		//	if (City.Instance.GetTile(Coordinates.x + 1, Coordinates.y) is IElectricGridElement rightGrid) { ConnectsFromRight = rightGrid; }
+		//	if (City.Instance.GetTile(Coordinates.x, Coordinates.y + 1) is IElectricGridElement belowGrid) { ConnectsFromBelow = belowGrid; }
+		//	if (City.Instance.GetTile(Coordinates.x - 1, Coordinates.y) is IElectricGridElement leftGrid) { ConnectsFromLeft = leftGrid; }
+
+		//	if (City.Instance.GetTile(Coordinates.x, Coordinates.y - 1) is Tile aboveTile) { aboveTile.OnTileDelete += TileDeleteHandler; }
+		//	if (City.Instance.GetTile(Coordinates.x + 1, Coordinates.y) is Tile rightTile) { rightTile.OnTileDelete += TileDeleteHandler; }
+		//	if (City.Instance.GetTile(Coordinates.x, Coordinates.y + 1) is Tile belowTile) { belowTile.OnTileDelete += TileDeleteHandler; }
+		//	if (City.Instance.GetTile(Coordinates.x - 1, Coordinates.y) is Tile leftTile) { leftTile.OnTileDelete += TileDeleteHandler; }
+		//}
+
+		private void TileDeleteHandler(object sender, Tile oldTile)
+		{
+			Tile newTile = City.Instance.GetTile(oldTile);
+			newTile.OnPreTileDelete += TileDeleteHandler;
+
+			if (newTile is IElectricGridElement pole)
+			{
+				if (oldTile.Coordinates.x < Coordinates.x) { ConnectsFromLeft = pole; }
+				else if (oldTile.Coordinates.x > Coordinates.x) { ConnectsFromRight = pole; }
+				else if (oldTile.Coordinates.y > Coordinates.y) { ConnectsFromBelow = pole; }
+				else if (oldTile.Coordinates.y < Coordinates.y) { ConnectsFromAbove = pole; }
+			}
+			else
+			{
+				if (oldTile.Coordinates.x < Coordinates.x) { ConnectsFromLeft = null; }
+				else if (oldTile.Coordinates.x > Coordinates.x) { ConnectsFromRight = null; }
+				else if (oldTile.Coordinates.y > Coordinates.y) { ConnectsFromBelow = null; }
+				else if (oldTile.Coordinates.y < Coordinates.y) { ConnectsFromAbove = null; }
+			}
+		}
+
+		public IElectricGridElement ConnectsFromAbove
+		{
+			get { return _poles[0]; }
+			private set
+			{
+				_poles[0] = value;
+				if (value == null) { DesignID &= ~ABOVEPOLEMASK; }
+				else { DesignID |= ABOVEPOLEMASK; }
+			}
+		}
+		public IElectricGridElement ConnectsFromRight
+		{
+			get { return _poles[1]; }
+			private set
+			{
+				_poles[1] = value;
+				if (value == null) { DesignID &= ~RIGHTPOLEMASK; }
+				else { DesignID |= RIGHTPOLEMASK; }
+			}
+		}
+		public IElectricGridElement ConnectsFromBelow
+		{
+			get { return _poles[2]; }
+			private set
+			{
+				_poles[2] = value;
+				if (value == null) { DesignID &= ~BELOWPOLEMASK; }
+				else { DesignID |= BELOWPOLEMASK; }
+			}
+		}
+		public IElectricGridElement ConnectsFromLeft
+		{
+			get { return _poles[3]; }
+			private set
+			{
+				_poles[3] = value;
+				if (value == null) { DesignID &= ~LEFTPOLEMASK; }
+				else { DesignID |= LEFTPOLEMASK; }
+			}
+		}
+
+		private ElectricGrid _electricGrid = null;
+
+		ElectricGrid IElectricGridElement.ElectricGrid
+		{
+			get => _electricGrid;
+			set
+			{
+				SetElectricGrid(value);
+			}
+		}
+
+		/// <summary>
+		/// Sets the new electricgrid
+		/// </summary>
+		/// <param name="electricGrid">New electricgrid</param>
+		private void SetElectricGrid(ElectricGrid electricGrid)
+		{
+			if (!_isFinalized) { throw new InvalidOperationException(); }
+
+			if (_electricGrid == electricGrid) { return; }
+
+			//List<Building> buildings = ElectricGridManager.GetBuildingsByElectricGridElement(this); TODO
+			//foreach (Building building in buildings)
+			//{
+			//	if (building is IWorkplace workplace)
+			//	{
+			//		workplace.UnregisterWorkplace(_electricGrid);
+			//	}
+			//	if (building is IResidential residential)
+			//	{
+			//		residential.UnregisterResidential(_electricGrid);
+			//	}
+			//}
+
+			if (electricGrid == null)
+			{
+				_electricGrid?.RemoveElectricGridElement(this);
+				_electricGrid?.Reinit();
+			}
+			else
+			{
+				_electricGrid?.RemoveElectricGridElement(this);
+				_electricGrid = electricGrid;
+				_electricGrid?.AddElectricGridElement(this);
+			}
+
+			//if (_electricGrid != null)
+			//{
+			//	foreach (Building building in buildings)
+			//	{
+			//		if (building is IWorkplace workplace)
+			//		{
+			//			workplace.RegisterWorkplace(_electricGrid);
+			//		}
+			//		if (building is IResidential residential)
+			//		{
+			//			residential.RegisterResidential(_electricGrid);
+			//		}
+			//	}
+			//}
+		}
+
+		private readonly List<Person> _lockedBy = new();
+		public bool IsLocked { get => _lockedBy.Count != 0; }
+		public void LockBy(Person person) { _lockedBy.Add(person); }
+		public void UnlockBy(Person person) { _lockedBy.Remove(person); }
+		#endregion
+
+		public const uint ABOVEPOLEMASK = 1;
+		public const uint RIGHTPOLEMASK = 2;
+		public const uint BELOWPOLEMASK = 4;
+		public const uint LEFTPOLEMASK = 8;
 
 		/// <summary>
 		/// Construct a new electric pole
